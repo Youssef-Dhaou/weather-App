86e595d521e8272da74c37f762dd02275b1058b1
Youssef-Dhaou <youssefdhaouu@gmail.com>
2022-09-15 14:35:30 +0100

    hey

--- src/JS/actions/weatherActions.js
+++ src/JS/actions/weatherActions.js
@@ -9,7 +9,7 @@ export const getAllWeather = (query) => async (dispatch) => {
   dispatch({ type: GET_WEATHER_lOADING });
   try {
     const response = await axios.get(
-      `https://api.weatherapi.com/v1/forecast.json?key=d2a566024b2847e5ad9213829221306&q=${query}&days=7&aqi=no&alerts=no`
+      `//api.weatherapi.com/v1/forecast.json?key=d2a566024b2847e5ad9213829221306&q=${query}&days=7&aqi=no&alerts=no`
     );
     dispatch({ type: GET_WEATHER_SUCCESS, payload: response.data });
   } catch (error) {
