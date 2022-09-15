b8302b46c7908d57f5f4382f7f9dbb49e9710d40
Youssef-Dhaou <youssefdhaouu@gmail.com>
2022-09-15 14:07:10 +0100

    first commit

---
+++ src/JS/store.js
@@ -0,0 +1,5 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { weatherReducer } from './reducers/weatherReducer'
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(weatherReducer, composeEnhancers(applyMiddleware(thunk)))
